411b54d73b96ee95fbc6e57e71e111073b254a11
Jon Nordling <jnordling@blueraster.com>
2015-06-23 14:26:12 -0400

    Something went wrong

--- app/src/app.js
+++ app/src/app.js
@@ -1,15 +1,11 @@
 import express from 'express'
-import chia from 'chia'
-import request from 'request'
 
-let http = request();
-let assert = chia();
 let app = express();
 
 app.get('/', function (req, res) {
-  //res.send('Hello World!');
-  var urlTest = test_api_access('https://api.fda.gov','/drug/enforcement.json')
-  res.send(urlTest);
+  res.send('Hello World!');
+  //var urlTest = test_api_access('https://api.fda.gov','/drug/enforcement.json')
+  //res.send(urlTest);
 });
 
 let server = app.listen(3000, function () {
@@ -17,29 +13,36 @@ let server = app.listen(3000, function () {
   let host = server.address().address;
   let port = server.address().port;
 
-  
-
 });
 
 
-function test_api_access(url,path){
-	// Make request to server
-	// Return's True of False 
+// function test_api_access(url,path){
+// 	// Make request to server
+// 	// Return's True of False 
+// 	// return http.get({
+// 	// 	host:url,
+// 	// 	path:path,
+// 	// },function(response){
+// 	// 	console.log(response);
+// 	// });
 
-	return http.get({
-		host:url,
-		path:path,
-	},function(response){
-		console.log(response);
-	});
+// 	return true;
 
 }
 
-function search_food(key,value){
-	return http.get({
-		host:url,
-		path:path,
-	},function(response){
-		console.log(response);
-	});
-}
+// function search_food(key,value){
+// 	return http.get({
+// 		host:url,
+// 		path:path,
+// 	},function(response){
+// 		console.log(response);
+// 	});
+// }
+// Ment to store the results if the data in not 
+// function store_new_results(){
+
+// }
+
+
+
+
