93014b31342172c2218740d35d79d44c2e1d4ee1
Jon Nordling <jnordling@blueraster.com>
2015-06-23 13:49:57 -0400

    Making a test

--- app/src/app.js
+++ app/src/app.js
@@ -1,10 +1,15 @@
 import express from 'express'
+import chia from 'chia'
+import request from 'request'
 
+let http = request();
+let assert = chia();
 let app = express();
 
-//var express = require('express');
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  //res.send('Hello World!');
+  var urlTest = test_api_access('https://api.fda.gov','/drug/enforcement.json')
+  res.send(urlTest);
 });
 
 let server = app.listen(3000, function () {
@@ -12,6 +17,20 @@ let server = app.listen(3000, function () {
   let host = server.address().address;
   let port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  
 
 });
+
+
+function test_api_access(url,path){
+	// Make request to server
+	// Return's True of False 
+
+	return http.get({
+		host:url,
+		path:path,
+	},function(response){
+		console.log(response);
+	});
+
+}
