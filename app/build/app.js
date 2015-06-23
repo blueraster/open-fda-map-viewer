22bb33e0a7ed24d6e4738622bcba6c7f2b05d254
Albert Marquez <amarquez@blueraster.com>
2015-06-23 13:16:36 -0400

    start script

--- app/build/app.js
+++ app/build/app.js
@@ -1,1 +1,22 @@
-"use strict";
+'use strict';
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+var app = (0, _express2['default'])();
+
+//var express = require('express');
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
