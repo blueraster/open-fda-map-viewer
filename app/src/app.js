5cad5a3262e28017d4ea43a80e9db1b16ae62ce7
Jon Nordling <jnordling@blueraster.com>
2015-06-23 13:00:03 -0400

    Setting up express server

---
+++ app/src/app.js
@@ -0,0 +1,17 @@
+import express from 'express'
+
+let app = express();
+
+//var express = require('express');
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+let server = app.listen(3000, function () {
+
+  let host = server.address().address;
+  let port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
+});
