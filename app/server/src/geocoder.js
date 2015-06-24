6daf37b831ad0727ed26b88114e2a9511a900a34
Jon Nordling <jnordling@blueraster.com>
2015-06-24 12:43:27 -0400

    Working on GeoCoder

--- app/server/src/geocoder.js
+++ app/server/src/geocoder.js
@@ -1,9 +1,10 @@
-import request from 'request'
+import request from 'request-promise'
+import util from 'util'
 import fs from 'fs'
 // TODO: get all api records
 // TODO: run geocode
 //https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/find?text=40%20East%20Rosemonts%20Alexandria%20va&maxLocations=10&outSR=102100&f=json
-//export default function (datajson) {
+export default function (datajson) {
   let data = [{
       "recall_number": "F-0283-2013",
       "reason_for_recall": "During an FDA inspection, microbiological swabs were collected and the results found that 21 sub samples in zones 1, 2 & 3 are positive for Listeria Monocytogenes (L.M.), Listeria innocua (L.I.) or Listeria seeligeri (L.S.).  The firm is voluntarily recalling all products manufactured from August 20th to September 10th 2012 due to the possible contamination.  All products with sell by dates on or before 11-OCT. No illnesses have been reported.",
@@ -27,31 +28,34 @@ import fs from 'fs'
       "openfda": {},
       "initial_firm_notification": "E-Mail",
     }];
-  var geocodeBaseUrl = 'https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/find?maxLocations=1&outSR=102100&f=json&text='
+  //var geocodeBaseUrl = 'https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/find?maxLocations=1&outSR=102100&f=json&text='
   var outPutJson = []
+  var maxLocations = '1';
+  var outSR = '102100';
 /// ToDo: Verify logic on name of city and state for one city and state
-    var stream = fs.createWriteStream("my_file.txt");
+    //var stream = fs.createWriteStream("my_file.txt");
     for(let d in data){
       let id = data[d]['@id'];
       let state = data[d]['state'];
       let country = data[d]['country'];
       let city = data[d]['city'];
-      let loction = city+' '+state +' ' + country;
-      let requestURL =  geocodeBaseUrl+encodeURIComponent(loction);
+      let loction = encodeURIComponent(city+' '+state +' ' + country);
+      //let requestURL =  geocodeBaseUrl+encodeURIComponent(loction) ;
+        request.get(`https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/find?maxLocations=${maxLocations}&outSR=${outSR}&f=json&text=${loction}`)
+          .then((results)=>{
+            console.log(results);
+          })
 
-      request
-        .get(requestURL)
-        .pipe(fs.createWriteStream('my_file.txt'))
 
-      request(requestURL, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-          let obj = JSON.parse(body);
-          let x = obj['locations'][0]['feature']['geometry']['x'];
-          let y = obj['locations'][0]['feature']['geometry']['y'];
-          outPutJson.push({'id':id,'x':x,'y':y});
-          console.log(outPutJson);
-        }
-      });
+      // request(requestURL, function (error, response, body) {
+      //   if (!error && response.statusCode == 200) {
+      //     let obj = JSON.parse(body);
+      //     let x = obj['locations'][0]['feature']['geometry']['x'];
+      //     let y = obj['locations'][0]['feature']['geometry']['y'];
+      //     outPutJson.push({'id':id,'x':x,'y':y});
+      //     console.log(outPutJson);
+      //   }
+      // });
 
     }
 
@@ -62,5 +66,5 @@ import fs from 'fs'
 
   //return true;
 
-//}
+}
 
