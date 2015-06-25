44adec4eb83c51649dc92269bc046602485c4154
Albert Marquez <amarquez@blueraster.com>
2015-06-25 17:58:25 -0400

    panel mock layout

--- app/client/src/app/js/app/app.js
+++ app/client/src/app/js/app/app.js
@@ -11,8 +11,29 @@ export class App extends React.Component {
   render () {
     return (
       <div className='app'>
-        <div className='app-body back-gray--dark'>
-          <Map />
+        <Map />
+        <div className='app__panel absolute no-right padding back-white fill__long'>
+          <div>Open FDA Enforcement MAPPER</div>
+          <div>
+            <select>
+              <option>Joe's Sauce</option>
+            </select>
+            <div>total event count</div>
+            <ul>
+              <li>event 1</li>
+              <li>event 2</li>
+              <li>event 3</li>
+              <li>event 4</li>
+            </ul>
+            <div>Event Details</div>
+            <div>
+              <ul>
+                <li>attribute</li>
+                <li>attribute</li>
+                <li>attribute</li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     )
