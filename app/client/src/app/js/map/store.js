b285ef69a415ec4901792b16ba551925e99caf43
Albert Marquez <amarquez@blueraster.com>
2015-06-24 23:52:17 -0400

    clusterLayer tweaks, code cleanup

--- app/client/src/app/js/map/store.js
+++ app/client/src/app/js/map/store.js
@@ -20,19 +20,19 @@ export const store = dispatcher.createStore(class {
     this.map = new esriMap(config.id, config.options)
     on.once(this.map, 'extent-change', (event) => {
       let clusterLayer = new ClusterFeatureLayer({
-          "url": "http://services.arcgis.com/oKgs2tbjK6zwTdvi/arcgis/rest/services/Major_World_Cities/FeatureServer/0",
-          "distance": 75,
-          "id": "clusters",
-          "labelColor": "#fff",
-          "resolution": this.map.extent.getWidth() / this.map.width,
-          // "singleTemplate": infoTemplate,
-          "useDefaultSymbol": false,
-          "zoomOnClick": true,
-          "showSingles": true,
-          "objectIdField": "FID",
-          outFields: ["NAME", "COUNTRY", "POPULATION", "CAPITAL"]
+          'url': 'http://services.arcgis.com/oKgs2tbjK6zwTdvi/arcgis/rest/services/Major_World_Cities/FeatureServer/0',
+          'distance': 75,
+          'id': 'clusters',
+          'labelColor': '#fff',
+          'resolution': this.map.extent.getWidth() / this.map.width,
+          // 'singleTemplate': infoTemplate,
+          'useDefaultSymbol': false,
+          // 'zoomOnClick': true,
+          'zoomOnClick': false,
+          'showSingles': true,
+          'objectIdField': 'FID',
+          outFields: ['NAME', 'COUNTRY', 'POPULATION', 'CAPITAL']
       });
-      window.temp = clusterLayer
       this.map.addLayer(clusterLayer);
       this.map.on('extent-change', (event) => {
         clusterLayer._reCluster()
