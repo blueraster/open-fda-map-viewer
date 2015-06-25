2f116e334d1c9219f13d118d4bb65b8991074eb8
Albert Marquez <amarquez@blueraster.com>
2015-06-24 23:46:51 -0400

    geoStore query route, began stubbing out functional tests for app.js

--- app/server/src/app.js
+++ app/server/src/app.js
@@ -3,9 +3,9 @@ import {allRecallsByTerm, recallByTerm} from './fetcher'
 import geocoder from './geocoder'
 import express from 'express'
 
-let app = express();
-let geoStore;
-let init;
+let app = express()
+let geoStore
+let init
 
 init = function (callback) {
   Promise.all([for (i of config.foodCategories) recallByTerm(i)])
@@ -18,14 +18,14 @@ init = function (callback) {
 }
 
 app.get('/', function (req, res) {
-  res.send('Open FDA Map Viewer');
-});
+  res.send('Open FDA Map Viewer')
+})
 
 // TODO: config port
 let server = app.listen(3000, function () {
-  let host = server.address().address;
-  let port = server.address().port;
-});
+  let host = server.address().address
+  let port = server.address().port
+})
 
 // TODO: setup route counditional on dev environment
 app.get('/init', function (req, res) {
@@ -38,9 +38,23 @@ app.get('/init', function (req, res) {
   } else {
     res.send(`Previously generated geoStore:\n\n${JSON.stringify(geoStore)}`)
   }
-});
+})
 
-app.get('/food',function(req,res){
+app.get('/getGeoData',function(req,res){
+  let food
 
-});
+  if (req.query.food === undefined) {
+    res.status(404).send('404: Bad Request - missing food query')
+  } else {
+    food = req.query.food.trim().toLowerCase()
+  }
+
+  if (geoStore === undefined) {
+    res.status(500).send('500: Server Error - Resources have not been initialized')
+  } else if (geoStore[food] === undefined) {
+    res.status(500).send('500: Server Error - Resource does not exist')
+  } else {
+    res.send(geoStore[food])
+  }
+})
 
---
+++ app/server/test/app.js
@@ -0,0 +1,3 @@
+// Functional test for app.js
+
+// TODO: test to generate geoStore as file to use for development
