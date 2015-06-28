2c420979adec0aa840690284b163616b7b71676a
Albert Marquez <amarquez@blueraster.com>
2015-06-27 20:33:52 -0400

    infoWindow behavior

--- app/client/src/app/js/map/infoWindowContent.js
+++ app/client/src/app/js/map/infoWindowContent.js
@@ -1,7 +1,9 @@
 import {store} from 'map/store'
-import {app as appConfig} from 'js/config'
+import {map as config, app as appConfig} from 'js/config'
 // lib/vendor/shim/esri/dojo
 import React from 'react'
+import graphicsUtils from 'esri/graphicsUtils'
+import on from 'dojo/on'
 
 export class InfoWindowContent extends React.Component {
   constructor (props) {
@@ -12,23 +14,46 @@ export class InfoWindowContent extends React.Component {
     let infoWindow = this.state.map.infoWindow,
         features = infoWindow.features,
         ui
+
     ui = infoWindow.features === null ? undefined : () => {
       let recallingFirms = Array.from(new Set([for (f of infoWindow.features) f.attributes.recalling_firm])),
           feature =  infoWindow.getSelectedFeature(),
           attributes = feature.attributes,
           recallDeatils = [for (label of appConfig.detailLabels) if (attributes[label.key] !== undefined) <div>{`${label.text}: ${attributes[label.key]}`}</div>]
-      console.debug(feature.attributes)
+
       return ([
         'Firms:',
-        recallingFirms.map((firmName) => <div><button>{firmName}</button></div>),
-        'Recall details:',
-        <div className='back-white border-black'>{recallDeatils}</div>
+        recallingFirms.map((firmName) => <div><button onClick={this.focusFirm.bind(this)}>{firmName}</button></div>),
       ])
     }()
     return (
       <div>
-        <div onClick={() => {console.debug("hey")}}>{ui}</div>
+        <div>{ui}</div>
       </div>
     )
+  }
+  focusFirm (event) {
+    let map = this.state.map,
+        infoWindow = map.infoWindow,
+        features = infoWindow.features,
+        feature = infoWindow.getSelectedFeature(),
+        firmFeatures = [for (f of features) if (f.attributes.recalling_firm === event.target.innerText) f],
+        recallsExtent = graphicsUtils.graphicsExtent(firmFeatures)
+
+    // map.setExtent(recallsExtent)
+    map.centerAndZoom(firmFeatures[0].geometry, config.options.maxZoom)
+    infoWindow.setFeatures(firmFeatures)
+    on.once(map, 'zoom-end', () => {infoWindow.reposition()})
+
+    // debugger
+    // console.debug(graphicsExtent(firmFeatures))
+    // map.setExtent(graphicsUtils.graphicsExtent(firmFeatures), true)
+    // filter features by firm event
+    // get extent of all features
+    // zoom to extent
+    // infoWindow.hide()
+    // set features
+    // infoWindow.show()
+    // debugger
   }
 }
