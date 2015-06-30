10d2bc4d1f20d06394fb152fa5036398b837de59
Albert Marquez <amarquez@blueraster.com>
2015-06-30 14:07:23 -0400

    welcome tweak, chartTitle file

---
+++ app/client/src/app/js/app/chartTitle.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export class ChartTitle extends React.Component {
+  render () {
+    return (
+      <div className='text-center'>%Food% Recalls for %Latest Year%</div>
+    )
+  }
+}
