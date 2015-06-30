fcd95b91265cddb8952827a020be851eff37b739
Albert Marquez <amarquez@blueraster.com>
2015-06-30 02:41:17 -0400

    map controls tweaks

--- app/client/src/app/js/map/controls.js
+++ app/client/src/app/js/map/controls.js
@@ -8,7 +8,7 @@ import Search from 'esri/dijit/Search'
 
 export class Controls extends React.Component {
   componentDidMount() {
-    new HomeButton({map: this.props.map}, 'home-button').startup()
+    // new HomeButton({map: this.props.map}, 'home-button').startup()
     // new LocateButton({map: this.props.map}, 'locate-button').startup()
   }
   render() {
