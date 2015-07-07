de19555dfd6d90d8688e672ea00aa5aa57d1addd
Albert Marquez <amarquez@blueraster.com>
2015-07-07 10:42:58 -0400

    style tweaks, title update

--- app/client/src/app/js/messages.js
+++ app/client/src/app/js/messages.js
@@ -1,6 +1,6 @@
 import {map as config} from 'js/config'
 export const messages = {
-  title: 'Open FDA Enforcement Mapper',
+  title: 'openFDA Enforcement Mapper',
   text: 'Agile Delivery Service Example Using openFDA Data: Geo-Enabling Enforcement Reports',
   subheader: 'Enforcement reports throughout the U.S. mentioning the items below',
   instructions: 'Select a product to view recalls:'
